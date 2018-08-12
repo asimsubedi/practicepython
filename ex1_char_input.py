24481877640b78a9f1266c0dc22c838f21dae4f1
Asim Subedi <asim232@gmail.com>
2018-08-12 13:51:54 -0700

    loop for given no. of times the message

--- ex1_char_input.py
+++ ex1_char_input.py
@@ -1,4 +1,6 @@
 username = input('what is your name?')
 userage = int(input('what is your age?'))
 centuryin = 100 - userage
-print('Hello Mr. ' + username +'. You will be 100 after ' + str(centuryin) + ' years')
+loopmsg = int(input('How Many times to display?'))
+for i in range(loopmsg):
+    print('Hello Mr. ' + username +'. You will be 100 after ' + str(centuryin) + ' years \n')
