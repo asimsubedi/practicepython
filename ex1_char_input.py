845d327a4cc599e97981629b23a1de930594466e
Asim Subedi <asim232@gmail.com>
2018-08-12 13:40:30 -0700

    exercise 1 initial done

---
+++ ex1_char_input.py
@@ -0,0 +1,4 @@
+username = input('what is your name?')
+userage = int(input('what is your age?'))
+centuryin = 100 - userage
+print('Hello Mr. ' + username +'. You will be 100 after ' + str(centuryin) + ' years')
